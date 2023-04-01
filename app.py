56fa153028f25ed0fa252390ff791e85c3cf3133
Realeed <hsargsyan.395@gmail.com>
2023-04-01 21:24:12 +0400

    m

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, send_file
+
+app = Flask(__name__)
+app.secret_key = "secret"
+
+@app.route("/", methods = ["POST", "GET"])
+def main():
+    return render_template('index.html')
+
+if __name__  == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
--- ptest.py
+++ ptest.py
@@ -1,3 +1,3 @@
 from pnp import convertFile
 
-convertFile('C:\\Users\\hsarg\\Downloads\\attachments\\Pick Place for Thin_Task_Lamp_Driver_RevF_Panel.csv')
+convertFile('C:\\Users\\hsarg\\Downloads\\attachments\\Pick Place for USB_TypeA_TypeC_Charger_RevB_Panel (2).csv')

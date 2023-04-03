5677b2c6183f95f2c2d1414231dc2d569652b395
Realeed <hsargsyan.395@gmail.com>
2023-04-04 00:25:26 +0400

    m

--- app.py
+++ app.py
@@ -28,6 +28,21 @@ def getExcel():
             excel.save(fullPaths[index])
             layers = convertSingle10(fullPaths[index])
 
+            if len(excels) > 1:
+                with ZipFile(dir + 'Formatted_excels.zip', 'w') as zipObj:
+                    for index, filename in enumerate(filenames):
+                        if layers == 2:
+                            with ZipFile(dir + filename + '.zip', 'w') as zipObj:
+                                zipObj.write(dir + filename.replace('Pick Place for ', '').replace('Panel', 'Single') + '_Top_N10' + extensions[index], 
+                                            arcname = filename.replace('Pick Place for ', '').replace('Panel', 'Single') + '_Top_N10' + extensions[index])
+                                zipObj.write(dir + filename.replace('Pick Place for ', '').replace('Panel', 'Single') + '_Bottom_N10' + extensions[index], 
+                                            arcname = filename.replace('Pick Place for ', '').replace('Panel', 'Single') + '_Bottom_N10' + extensions[index])
+                        elif layers == 1:
+                             with ZipFile(dir + filename + '.zip', 'w') as zipObj:
+                                zipObj.write(dir + filename.replace('Pick Place for ', '').replace('Panel', 'Single') + '_Top_N10' + extensions[index], 
+                                            arcname = filename.replace('Pick Place for ', '').replace('Panel', 'Single') + '_Top_N10' + extensions[index])
+                        return send_file(dir + 'Formatted_excels.zip')
+                
             if layers == 2:
                 with ZipFile(dir + filenames[0] + '.zip', 'w') as zipObj:
                     zipObj.write(dir + filenames[0].replace('Pick Place for ', '').replace('Panel', 'Single') + '_Top_N10' + extensions[0], 
@@ -37,13 +52,6 @@ def getExcel():
                 return send_file(dir + filenames[0] + '.zip')
             elif layers == 1:
                 return send_file(dir + filenames[0].replace('Pick Place for ', '').replace('Panel', 'Single') + '_Top_N10' + extensions[0])
-            # if len(excels) > 1:
-            #     with ZipFile(dir + 'Formatted_excels.zip', 'w') as zipObj:
-            #         for index, filename in enumerate(filenames):
-            #             zipObj.write(fullPaths[index], arcname = filename + extensions[index])
-            #     return send_file(dir + 'Formatted_excels.zip')
-                
-           # return send_file(fullPaths[0], as_attachment=True)
 
 if __name__  == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)

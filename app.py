23fb655edef90908a38e97575aa1e19f6a38f583
Realeed <hsargsyan.395@gmail.com>
2023-04-05 10:08:11 +0400

    m

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def getExcel():
             return send_file(dir + filename.replace('Pick Place for ', '').replace('_N10', '') + '_Top_N10' + extension)
 
 if __name__  == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80)
